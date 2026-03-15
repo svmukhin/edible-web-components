82972776bbaace4ce3c3f1f6b6af349fed8d5fec
Sergei Mukhin <sergei_mukhin@outlook.com>
2026-03-15 18:42:10 +0300

    chore: use oxc to minify

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         format === 'es' ? 'edible-wc.esm.js' : 'edible-wc.js',
     },
     emptyOutDir: true,
-    minify: 'esbuild',
+    minify: 'oxc',
     cssCodeSplit: false,
   },
 
